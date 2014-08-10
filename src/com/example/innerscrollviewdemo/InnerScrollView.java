ceb13da464c4e3bb9afba047b4a5ba40d3e6513e
tianshiaimili <913540483@qq.com>
2014-08-10 22:12:39 +0800

    肉味儿

--- src/com/example/innerscrollviewdemo/InnerScrollView.java
+++ src/com/example/innerscrollviewdemo/InnerScrollView.java
@@ -6,7 +6,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
-
+///////test github
 public class InnerScrollView extends ScrollView{
 
 	  Handler handler;
